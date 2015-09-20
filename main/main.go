3eee7aad5012691687445568687785629c0491f0
Koichi Shiraishi <zchee.io@gmail.com>
2015-09-21 05:38:10 +0900

    Fix args and Add daemonize method - Change args to similar to xhyve flags - Daemonize execute itself when '-d' flag
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- main/main.go
+++ main/main.go
@@ -1,27 +1,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
 	"os"
+	"os/exec"
+	"runtime"
 	"strings"
 
 	"github.com/tiborvass/xhyve-bindings"
 )
 
 func usage() {
-	fmt.Println("Usage: ./main <vmlinuz> <initrd>")
+	fmt.Println("Usage: ./main <uuid> <iso> <img> <userdata> <vmlinuz> <initrd>")
 	os.Exit(1)
 }
 
 func main() {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 7 {
 		usage()
 	}
-	vmlinuz := os.Args[1]
-	initrd := os.Args[2]
-	args := strings.Fields("-m 1G -s 0:0,hostbridge -s 31,lpc -l com1,stdio")
-	if err := xhyve.Exec(append(args, "-f", fmt.Sprintf("kexec,%s,%s,earlyprintk=serial console=ttyS0", vmlinuz, initrd))...); err != nil {
-		log.Fatal(err)
+
+	// TODO GOMAXPROCS is necessary?
+	cpus := runtime.NumCPU()
+
+	runtime.GOMAXPROCS(cpus)
+
+	//TODO Summarize the argument
+	// xhyve like args
+	args := strings.Fields("-A -s 0:0,hostbridge -s 31,lpc -l com1 -s 2:0,virtio-net")
+	uuid := os.Args[1]
+	cpu := os.Args[2]
+	memory := os.Args[3]
+	iso := os.Args[4]
+	img := os.Args[5]
+	userdata := os.Args[6]
+	kexec := os.Args[7]
+
+	d := flag.Bool("d", false, "Whether or not to launch in the background(like a daemon)")
+	flag.Parse()
+
+	// When '-d' flag, execute itself the else below command
+	if *d {
+		// If did not call the gorutine, xhyve does not running
+		go func() {
+			cmd := exec.Command("sudo", os.Args[0])
+			fmt.Print(cmd)
+			cmd.Run()
+		}()
+	} else {
+		xhyve.Exec(append(args,
+			"-U", fmt.Sprintf("%s", uuid),
+			fmt.Sprintf("-c %sM", cpu),
+			fmt.Sprintf("-m %sM", memory),
+			fmt.Sprintf("-s 3,ahci-cd,%s", iso),
+			fmt.Sprintf("-s 4,virtio-blk,%s", img),
+			fmt.Sprintf("-s 5,virtio-blk,%s", userdata),
+			"-f", fmt.Sprintf("%s", kexec))...)
 	}
 }
